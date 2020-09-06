092ea696594dbaea53998b08fda74c41c4440dfc
Piotr Zieliński <piotrekzie100@gmail.com>
2020-09-06 16:30:25 +0200

    Add placeholders for models

---
+++ ssdir/__init__.py
@@ -0,0 +1,3 @@
+from ssdir.modeling.models import Decoder, Encoder, pyro_guide, pyro_model
+
+__all__ = ["Encoder", "Decoder", "pyro_model", "pyro_guide"]
---
+++ ssdir/modeling/models.py
@@ -0,0 +1,20 @@
+"""SSDIR encoder, decoder, model and guide declarations."""
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+def pyro_model():
+    pass
+
+
+def pyro_guide():
+    pass
