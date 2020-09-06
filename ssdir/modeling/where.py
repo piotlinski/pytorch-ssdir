11a0b2fc666a15fb0f276c0557d68f576176f164
Piotr Zieliński <piotrekzie100@gmail.com>
2020-09-06 23:59:17 +0200

    Test transformer output shape

--- ssdir/modeling/where.py
+++ ssdir/modeling/where.py
@@ -99,14 +99,26 @@ class WhereTransformer(nn.Module):
     def forward(
         self, decoded_images: torch.Tensor, where_boxes: torch.Tensor
     ) -> torch.Tensor:
+        """ Takes decoded images (batch_size x sum_features(grid*grid) x 3 x 64 x 64)
+        .. and bounding box parameters x_center, y_center, w, h tensor
+        .. (batch_size x sum_features(grid*grid) x 4)
+        .. and outputs transformed images
+        .. (batch_size x sum_features(grid*grid) x 3 x image_size x image_size)
+        """
         batch_size = decoded_images.shape[0]
-        channels = decoded_images.shape[1]
-        n_boxes = where_boxes.shape[1]
+        n_objects = decoded_images.shape[1]
+        channels = decoded_images.shape[2]
+        image_size = decoded_images.shape[3:]
         sxy = self.convert_boxes_to_sxy(where_boxes=where_boxes)
-        theta = self.expand_where(sxy).view(batch_size * n_boxes, -1)
+        theta = self.expand_where(sxy).view(batch_size * n_objects, 2, 3)
         grid = functional.affine_grid(
             theta=theta,
-            size=(batch_size * n_boxes, channels, self.image_size, self.image_size),
+            size=(batch_size * n_objects, channels, self.image_size, self.image_size),
         )
-        transformed_image = functional.grid_sample(input=decoded_images, grid=grid)
-        return transformed_image
+        transformed_images = functional.grid_sample(
+            input=decoded_images.view(batch_size * n_objects, channels, *image_size),
+            grid=grid,
+        )
+        return transformed_images.view(
+            batch_size, n_objects, channels, self.image_size, self.image_size
+        )
--- tests/test_where.py
+++ tests/test_where.py
@@ -1,5 +1,8 @@
 """Test where modules."""
-from ssdir.modeling.where import WhereEncoder
+import pytest
+import torch
+
+from ssdir.modeling.where import WhereEncoder, WhereTransformer
 
 
 def test_where_encoder_dimensions(ssd_config, ssd_model, ssd_features):
@@ -13,3 +16,18 @@ def test_where_encoder_dimensions(ssd_config, ssd_model, ssd_features):
         )
     )
     assert outputs.shape == (ssd_features[0].shape[0], n_boxes, 4)
+
+
+@pytest.mark.parametrize("decoded_size", [2, 3])
+@pytest.mark.parametrize("image_size", [7, 8])
+@pytest.mark.parametrize("batch_size", [3, 4])
+@pytest.mark.parametrize("hidden_size", [5, 6])
+def test_where_transformer_dimensions(
+    decoded_size, image_size, batch_size, hidden_size
+):
+    """Verify WhereTransformer output dimensions."""
+    decoded_images = torch.rand(batch_size, hidden_size, 3, decoded_size, decoded_size)
+    z_where = torch.rand(batch_size, hidden_size, 4)
+    transformer = WhereTransformer(image_size=image_size)
+    outputs = transformer(decoded_images, z_where)
+    assert outputs.shape == (batch_size, hidden_size, 3, image_size, image_size)
