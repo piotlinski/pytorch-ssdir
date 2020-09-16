2cc74c3536a5547d38f87b745659d2e58d030ee4
Piotr Zieliński <piotrekzie100@gmail.com>
2020-09-16 11:49:14 +0200

    Integrate horovod for distributed training

--- ssdir/cli.py
+++ ssdir/cli.py
@@ -2,17 +2,20 @@
 import logging
 
 import click
+import horovod.torch as hvd
 import numpy as np
 import pyro.optim as optim
 import torch
 from pyro.infer import SVI, Trace_ELBO
 from pyssd.config import get_config
-from pyssd.data.loaders import TrainDataLoader
+from pyssd.data.datasets import datasets
+from pyssd.data.transforms import SSDTargetTransform, TrainDataTransform
+from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from tqdm.auto import tqdm, trange
+from tqdm.auto import tqdm
 
 from ssdir import SSDIR
-from ssdir.modeling.utils import per_param_lr
+from ssdir.modeling.utils import HorovodOptimizer, per_param_lr
 
 logger = logging.getLogger(__name__)
 
@@ -59,6 +62,9 @@ def train(
     logging_step: int,
 ):
     """Train the model."""
+    hvd.init()
+    if device == "cuda":
+        torch.cuda.set_device(hvd.local_rank())
     epoch_loss = float("nan")
     logging_loss = float("nan")
     logging_losses = []
@@ -66,10 +72,6 @@ def train(
     tb_writer = SummaryWriter(log_dir=tb_dir)
 
     ssd_config = get_config(config_file=ssd_config_file)
-    ssd_config.defrost()
-    ssd_config.RUNNER.BATCH_SIZE = bs
-
-    train_loader = TrainDataLoader(config=ssd_config)
 
     global_step = 0
 
@@ -79,64 +81,77 @@ def train(
             lr_dict={"z_where": 1e-6 / 100, "z_present": lr / 10}, default_lr=lr
         )
     )
+    dataset = datasets[ssd_config.DATA.DATASET](
+        f"{ssd_config.ASSETS_DIR}/{ssd_config.DATA.DATASET_DIR}",
+        data_transform=TrainDataTransform(ssd_config),
+        target_transform=SSDTargetTransform(ssd_config),
+        subset="train",
+    )
+    sampler = torch.utils.data.distributed.DistributedSampler(
+        dataset=dataset, num_replicas=hvd.size(), rank=hvd.rank()
+    )
+    train_loader = DataLoader(
+        dataset=dataset,
+        num_workers=ssd_config.RUNNER.NUM_WORKERS,
+        sampler=sampler,
+        pin_memory=ssd_config.RUNNER.PIN_MEMORY,
+        batch_size=bs,
+    )
+
+    hvd.broadcast_parameters(model.state_dict(), root_rank=0)
+    optimizer = HorovodOptimizer(optimizer)
+
     loss_fn = Trace_ELBO()
     svi = SVI(model=model.model, guide=model.guide, optim=optimizer, loss=loss_fn)
 
-    add_graph(model=model, tb_dir=tb_dir, device=device)
+    epochs = range(n_epochs)
+    if hvd.rank() == 0:
+        epochs = tqdm(
+            epochs,
+            desc="TRAINING",
+            unit="epoch",
+            postfix=dict(step=global_step, loss=epoch_loss),
+        )
 
-    with trange(
-        n_epochs,
-        desc="TRAINING",
-        unit="epoch",
-        postfix=dict(step=global_step, loss=epoch_loss),
-    ) as epoch_pbar:
-        for epoch in epoch_pbar:
-            epoch_losses = []
-            epoch += 1
+    for epoch in epochs:
+        sampler.set_epoch(epoch)
+        epoch_losses = []
+        epoch += 1
 
-            with tqdm(
-                train_loader,
+        steps = train_loader
+        if hvd.rank() == 0:
+            steps = tqdm(
+                steps,
                 desc=f"  epoch {epoch:4d}",
                 unit="step",
                 postfix=dict(loss=logging_loss),
-            ) as step_pbar:
-                for images, _, _ in step_pbar:
-                    images = images.to(device)
-                    loss = svi.step(images)
+            )
+        for images, _, _ in steps:
+            images = images.to(device)
+            loss = svi.step(images)
 
-                    epoch_losses.append(loss)
-                    logging_losses.append(loss)
+            loss = hvd.allreduce(torch.tensor(loss), "loss")
+            loss = loss.item()
 
-                    if global_step % logging_step == 0:
-                        epoch_loss = np.average(epoch_losses)
-                        logging_loss = np.average(logging_losses)
-                        logging_losses = []
+            epoch_losses.append(loss)
+            logging_losses.append(loss)
 
-                        tb_writer.add_scalar(
-                            tag="elbo/train",
-                            scalar_value=logging_loss,
-                            global_step=global_step,
-                        )
+            if global_step % logging_step == 0 and hvd.rank() == 0:
+                epoch_loss = np.average(epoch_losses)
+                logging_loss = np.average(logging_losses)
+                logging_losses = []
 
-                        epoch_pbar.set_postfix(step=global_step, loss=epoch_loss)
-                        step_pbar.set_postfix(loss=loss)
+                tb_writer.add_scalar(
+                    tag="elbo/train",
+                    scalar_value=logging_loss,
+                    global_step=global_step,
+                )
 
-                    global_step += 1
+                epochs.set_postfix(step=global_step, loss=epoch_loss)  # type: ignore
+                steps.set_postfix(loss=loss)  # type: ignore
 
+            global_step += 1
 
-def add_graph(model: SSDIR, tb_dir: str, device: str = "cpu"):
-    """Write encoder and decoder graphs to TB."""
-    SummaryWriter(log_dir=f"{tb_dir}_encoder").add_graph(
-        model.encoder, torch.zeros((1, 3, 300, 300), device=device)
-    )
-    SummaryWriter(log_dir=f"{tb_dir}_decoder").add_graph(
-        model.decoder,
-        (
-            (
-                torch.zeros((1, model.n_objects, model.z_what_size), device=device),
-                torch.zeros((1, model.n_ssd_features, 4), device=device),
-                torch.ones((1, model.n_ssd_features, 1), device=device),
-                torch.ones((1, model.n_objects, 1), device=device),
-            ),
-        ),
-    )
+    hvd.shutdown()
+    if hvd.rank() != 0:
+        return
