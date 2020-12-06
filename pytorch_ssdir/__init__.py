808695f9768c99d434554c8c156568afda0c169f
Piotr Zieliński <piotrekzie100@gmail.com>
2020-12-06 19:37:03 +0100

    Add train.py file for basic docker setup

--- pytorch_ssdir/__init__.py
+++ pytorch_ssdir/__init__.py
@@ -1,3 +1,4 @@
+from pytorch_ssdir.main import cli
 from pytorch_ssdir.modeling.models import SSDIR, Decoder, Encoder
 
-__all__ = ["Decoder", "Encoder", "SSDIR"]
+__all__ = ["Decoder", "Encoder", "SSDIR", "cli"]
