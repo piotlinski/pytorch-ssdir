41867a0bb1ccad3772c184117bd58f42489f9383
PiotrJZielinski <piotrekzie100@gmail.com>
2021-03-02 14:17:10 +0100

    Drop too small boxes on transforming gt boxes

--- pytorch_ssdir/run/transforms.py
+++ pytorch_ssdir/run/transforms.py
@@ -11,8 +11,17 @@ def corner_to_center_target_transform(
     boxes: Union[np.ndarray, torch.Tensor], labels: Union[np.ndarray, torch.Tensor]
 ):
     """Convert ground truth boxes from corner to center form."""
+    boxes_mask_w = boxes[:, 2] - boxes[:, 0] > 0.04
+    boxes_mask_h = boxes[:, 3] - boxes[:, 1] > 0.04
+    boxes_mask = torch.logical_and(boxes_mask_w, boxes_mask_h)
+    boxes = boxes[boxes_mask.unsqueeze(-1).expand_as(boxes)].view(-1, 4)
+    labels = labels[boxes_mask]
+    if boxes.numel() == 0:
+        return torch.tensor([]), torch.tensor([])
+
     n_objs = boxes.shape[0]
     pad_size = 200
+
     return (
         functional.pad(
             corner_bbox_to_center_bbox(boxes).view(-1, 4), [0, 0, 0, pad_size - n_objs]
