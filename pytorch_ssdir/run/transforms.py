b95640b0161a015c8469c19ef2a22281cc2aa4db
PiotrJZielinski <piotrekzie100@gmail.com>
2021-03-02 14:24:00 +0100

    Enable dropping too small objects

--- pytorch_ssdir/run/transforms.py
+++ pytorch_ssdir/run/transforms.py
@@ -8,14 +8,18 @@ from pytorch_ssd.data.bboxes import corner_bbox_to_center_bbox
 
 
 def corner_to_center_target_transform(
-    boxes: Union[np.ndarray, torch.Tensor], labels: Union[np.ndarray, torch.Tensor]
+    boxes: Union[np.ndarray, torch.Tensor],
+    labels: Union[np.ndarray, torch.Tensor],
+    drop_too_small: bool = False,
 ):
     """Convert ground truth boxes from corner to center form."""
-    boxes_mask_w = boxes[:, 2] - boxes[:, 0] > 0.04
-    boxes_mask_h = boxes[:, 3] - boxes[:, 1] > 0.04
-    boxes_mask = torch.logical_and(boxes_mask_w, boxes_mask_h)
-    boxes = boxes[boxes_mask.unsqueeze(-1).expand_as(boxes)].view(-1, 4)
-    labels = labels[boxes_mask]
+    if drop_too_small:
+        boxes_mask_w = boxes[:, 2] - boxes[:, 0] > 0.04
+        boxes_mask_h = boxes[:, 3] - boxes[:, 1] > 0.04
+        boxes_mask = torch.logical_and(boxes_mask_w, boxes_mask_h)
+        boxes = boxes[boxes_mask.unsqueeze(-1).expand_as(boxes)].view(-1, 4)
+        labels = labels[boxes_mask]
+
     if boxes.numel() == 0:
         return torch.tensor([]), torch.tensor([])
 
